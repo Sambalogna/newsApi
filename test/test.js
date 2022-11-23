0464b3e4052bb82dbf53c6f083e3042fbdc51d8a
Sambalogna <smandel4@gmail.com>
2022-11-22 19:24:06 -0800

    test.js filtration complete

--- test/test.js
+++ test/test.js
@@ -14,19 +14,6 @@ const testArr = [
     },
     {
         "source": {
-            "id": null,
-            "name": "BBC News"
-        },
-        "author": "https://www.facebook.com/bbcnews",
-        "title": "Indonesia earthquake: Search for survivors as death toll soars - BBC",
-        "description": "At least 268 people, many of them children, are reported dead on Java island, with many still missing.",
-        "url": "https://www.bbc.com/news/world-asia-63712461",
-        "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/146C5/production/_127735638_workbuilding.png",
-        "publishedAt": "2022-11-22T19:56:52Z",
-        "content": "Rescuers in Indonesia are working through a second night to try to find survivors trapped following an earthquake in West Java. \r\nOfficials put the death toll at 268, many of them children, with 151 … [+3423 chars]"
-    },
-    {
-        "source": {
             "id": "fox-news",
             "name": "Fox News"
         },
@@ -64,228 +51,43 @@ const testArr = [
         "publishedAt": "2022-11-22T18:20:00Z",
         "content": "A substantial amount of failed crypto exchange FTXs assets is missing and may have been stolen as a run on customer deposits and a liquidity crunch precipitated a crisis of leadership and led the fir… [+384 chars]"
     },
-    {
-        "source": {
-            "id": null,
-            "name": "Profootballhof.com"
-        },
-        "author": null,
-        "title": "28 Modern-Era Players named as Semifinalists for Pro Football Hall of Fame's Class of 2023 - Pro Football Hall of Fame",
-        "description": "Five first-year eligible nominees are among the 28 Modern-Era Player Semifinalists for the Pro Football Hall of Fame’s Class of 2023.",
-        "url": "https://www.profootballhof.com/news/2022/28-modern-era-players-named-as-semifinalists-for-pro-football-hall-of-fame%E2%80%99s-class-of-2023/",
-        "urlToImage": "/pfhof/media/Assets/Hall-of-Fame-Logo_OG-image_1200x630.jpg",
-        "publishedAt": "2022-11-22T18:13:07Z",
-        "content": "<ul><li>5 first-year eligible candidates on list trimmed from 129 Nominees\r\n</li></ul>CANTON, OHIO – Five first-year eligible nominees are among the 28 Modern-Era Player Semifinalists for the Pro Foo… [+8782 chars]"
-    },
-    {
-        "source": {
-            "id": "cnn",
-            "name": "CNN"
-        },
-        "author": "Alta Spells, Jamiel Lynch",
-        "title": "Todd and Julie Chrisley sentenced for fraud and tax crimes convictions - CNN",
-        "description": "Reality TV Stars Julie and Todd Chrisley were sentenced to prison in federal court Monday.",
-        "url": "https://www.cnn.com/2022/11/21/entertainment/todd-chrisley-julie-chrisley-sentenced/index.html",
-        "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/221121192826-julie-and-todd-chrisley-file-restricted-081720.jpg?c=16x9&q=w_800,c_fill",
-        "publishedAt": "2022-11-22T18:13:00Z",
-        "content": "Reality TV Stars Julie and Todd Chrisley were sentenced to prison in federal court Monday. \r\nThe Chrisley Knows Best couple were found guilty in June of conspiracy to defraud banks out of more than $… [+1504 chars]"
-    },
-    {
-        "source": {
-            "id": null,
-            "name": "CNBC"
-        },
-        "author": "Spencer Kimball",
-        "title": "Fauci says he never imagined Covid would kill a million Americans - CNBC",
-        "description": "Dr. Anthony Fauci during a White House news conference said it pains him as a physician to see people refuse the Covid vaccine for political reasons.",
-        "url": "https://www.cnbc.com/2022/11/22/fauci-says-he-never-imagined-covid-would-kill-million-americans.html",
-        "urlToImage": "https://image.cnbcfm.com/api/v1/image/107155821-16691445002022-11-22t181355z_634123253_rc24rx9v60af_rtrmadp_0_usa-biden.jpeg?v=1669147282&w=1920&h=1080",
-        "publishedAt": "2022-11-22T18:08:53Z",
-        "content": "Dr. Anthony Fauci on Tuesday reflected on the U.S. response to the Covid-19 pandemic in what was likely his last public briefing as the nation's top infectious disease expert.\r\nNearly three years aft… [+3405 chars]"
-    },
-    {
-        "source": {
-            "id": null,
-            "name": "CNBC"
-        },
-        "author": "Sarah Whitten",
-        "title": "Taylor Swift's tour promoter says it had no choice but to work with Ticketmaster - CNBC",
-        "description": "AEG Presents, the company in charge of handling Taylor Swift's upcoming tour, said it didn't have any choice but to work with Ticketmaster.",
-        "url": "https://www.cnbc.com/2022/11/22/taylor-swift-tour-promoter-had-to-work-with-ticketmaster.html",
-        "urlToImage": "https://image.cnbcfm.com/api/v1/image/107155712-1669135489995-gettyimages-1443142868-_w3_1492_26f53b44-8f50-4634-8059-d087e55e8235.jpeg?v=1669140259&w=1920&h=1080",
-        "publishedAt": "2022-11-22T18:04:19Z",
-        "content": "Taylor Swift's tour promoter is shifting blame for the botched \"Eras\" ticket sale squarely onto Ticketmaster, potentially fueling even more concerns about the Live Nation-owned ticket seller's domina… [+1722 chars]"
-    },
-    {
-        "source": {
-            "id": "cnn",
-            "name": "CNN"
-        },
-        "author": "Frank Pallotta",
-        "title": "Here's how much Disney is going to pay Bob Iger - CNN",
-        "description": "Bob Iger, who shocked the media world when he returned as CEO of Disney on Sunday, will once again be among the highest-paid executives in Hollywood.",
-        "url": "https://www.cnn.com/2022/11/22/media/disney-bob-iger-pay/index.html",
-        "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/191106143740-03-risk-takers-series-bob-iger-disney.jpg?q=x_3,y_186,h_1684,w_2993,c_crop/w_800",
-        "publishedAt": "2022-11-22T18:04:00Z",
-        "content": "Bob Iger, who shocked the media world when he returned as CEO of Disney on Sunday, will once again be among the highest-paid executives in Hollywood. \r\nIger will earn a $1 million base salary as he t… [+1823 chars]"
-    },
-    {
-        "source": {
-            "id": "buzzfeed",
-            "name": "Buzzfeed"
-        },
-        "author": "Natasha Jokic",
-        "title": "Kelly Rowland Defended Chris Brown Again After Her AMAs Comments, And I Am Exhausted - BuzzFeed",
-        "description": "“I believe that grace is very real and we all need a dose of it.\"",
-        "url": "https://www.buzzfeed.com/natashajokic1/kelly-rowland-defended-chris-brown-again-after-her-amas",
-        "urlToImage": "https://img.buzzfeed.com/buzzfeed-static/static/2022-11/22/17/enhanced/1138ccf89162/original-1141-1669138479-14.jpg?crop=1246:652;3,27%26downsize=1250:*",
-        "publishedAt": "2022-11-22T17:57:31Z",
-        "content": "If you or someone you know is in immediate danger as a result of domestic violence, call 911. For anonymous, confidential help, you can call the 24/7 National Domestic Violence Hotline at 1-800-799-7… [+51 chars]"
-    },
-    {
-        "source": {
-            "id": "ars-technica",
-            "name": "Ars Technica"
-        },
-        "author": "Jon Brodkin",
-        "title": "Musk recruits engineers for “Twitter 2.0” after mass layoffs and resignations - Ars Technica",
-        "description": "Musk: \"People who are great at writing software are the highest priority.\"",
-        "url": "https://arstechnica.com/tech-policy/2022/11/musk-recruits-engineers-for-twitter-2-0-after-mass-layoffs-and-resignations/",
-        "urlToImage": "https://cdn.arstechnica.net/wp-content/uploads/2022/11/getty-musk-twitter-760x380.jpg",
-        "publishedAt": "2022-11-22T17:34:39Z",
-        "content": "173 with 0 posters participating\r\nHaving gutted Twitter's staff, Elon Musk told remaining employees he plans to hire new engineers and salespeople, The Verge reported Monday. \"During an all-hands mee… [+2674 chars]"
-    },
-    {
-        "source": {
-            "id": null,
-            "name": "Arrowhead Pride"
-        },
-        "author": "John Dixon",
-        "title": "NFL Power Rankings Week 12 roundup: Chiefs almost the consensus pick - Arrowhead Pride",
-        "description": "Let’s see where Kansas City stands in the Week 12 NFL power rankings.",
-        "url": "https://www.arrowheadpride.com/2022/11/22/23473389/nfl-power-rankings-week-12-roundup-chiefs-almost-the-consensus-pick",
-        "urlToImage": "https://cdn.vox-cdn.com/thumbor/sU4jpeBeZ6BgQ2XvCpFRUKEAVxs=/0x135:2693x1545/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/24219994/1443153033.jpg",
-        "publishedAt": "2022-11-22T17:29:24Z",
-        "content": "You might call it a consensus with outliers but according to this weeks national power rankings, the Kansas City Chiefs are the NFLs best team. Well just have to wait for a couple of these folks to c… [+4232 chars]"
-    },
-    {
-        "source": {
-            "id": "abc-news",
-            "name": "ABC News"
-        },
-        "author": "Emily Shapiro, Jennifer Leong, Jenner Smith",
-        "title": "Delphi murders: Judge to decide whether documents should be unsealed - ABC News",
-        "description": "Prosecutors want to keep the probable cause affidavit sealed.",
-        "url": "https://abcnews.go.com/US/delphi-murder-case-court-documents-unsealed/story?id=93741287",
-        "urlToImage": "https://s.abcnews.com/images/US/richard-allen-delphi-ho-mo-20221031_1667227297038_hpMain_2_16x9_992.jpg",
-        "publishedAt": "2022-11-22T16:52:30Z",
-        "content": "A judge presiding over the Delphi, Indiana, double murder case is reviewing arguments to decide whether the probable cause affidavit and other documents related to suspect Richard Allen's arrest shou… [+2188 chars]"
-    },
-    {
-        "source": {
-            "id": "cbs-news",
-            "name": "CBS News"
-        },
-        "author": "CBSBoston.com Staff",
-        "title": "Bradley Rein, driver charged in deadly crash at Hingham Apple store, says it was an accident - CBS Boston",
-        "description": "According to prosecutors, Bradley Rein told investigators he was looking for an eyeglass store when his right foot became stuck on accelerator.",
-        "url": "https://www.cbsnews.com/boston/news/bradley-rein-arrested-apple-store-crash-hingham-massachusetts-driver-charged/",
-        "urlToImage": "https://assets1.cbsnewsstatic.com/hub/i/r/2022/11/22/a6404def-6b99-4266-9172-c51d47fb45f5/thumbnail/1200x630/41077a38a398a434f6a4ad0b78dec0bb/rein1.jpg",
-        "publishedAt": "2022-11-22T16:44:00Z",
-        "content": "HINGHAM - Bradley Rein, the driver in the deadly crash at an Apple store in Hingham, is now charged with reckless homicide by motor vehicle and reckless operation of a motor vehicle. His attorney sai… [+2592 chars]"
-    },
-    {
-        "source": {
-            "id": "ign",
-            "name": "IGN"
-        },
-        "author": "Anthony Wood",
-        "title": "Pokémon Scarlet and Violet Duplication Glitch Lets You Clone Shiny Pokémon - IGN",
-        "description": "An exploit has been found in Pokémon Scarlet and Violet that lets you clone Pokémon, including shiny ones.",
-        "url": "https://www.ign.com/articles/pokemon-scarlet-and-violet-duplication-glitch-clone-shiny-pokemon",
-        "urlToImage": "https://assets-prd.ignimgs.com/2022/10/25/pokemonscarletvioletlandstowns-blog-1666697068432.jpg?width=1280",
-        "publishedAt": "2022-11-22T16:37:46Z",
-        "content": "A newly discovered glitch in Pokémon Scarlet and Violet allows the player to very simply duplicate Pokémon, including the incredibly rare shiny ones.\r\nAs reported by Nintendo Everything, YouTuber Aus… [+2056 chars]"
-    },
-    {
-        "source": {
-            "id": null,
-            "name": "SamMobile"
-        },
-        "author": "SamMobile",
-        "title": "Samsung offers an outstanding Galaxy Z Fold 4 Black Friday deal - SamMobile - Samsung news",
-        "description": "If you're among the many Samsung fans who have waited for the opportunity to buy a new Galaxy foldable phone ...",
-        "url": "https://www.sammobile.com/news/samsung-outstanding-galaxy-z-fold-4-black-friday-deal/",
-        "urlToImage": "https://www.sammobile.com/wp-content/uploads/2022/08/Galaxy-Z-Fold-4-9-1-720x405.jpg",
-        "publishedAt": "2022-11-22T16:26:00Z",
-        "content": "If you’re among the many Samsung fans who have waited for the opportunity to buy a new Galaxy foldable phone for the price of a Galaxy S flagship, you’re in luck. Samsung has an outstanding Galaxy Z … [+2067 chars]"
-    },
-    {
-        "source": {
-            "id": null,
-            "name": "Space.com"
-        },
-        "author": "Tereza Pultarova",
-        "title": "James Webb Space Telescope reveals alien planet's atmosphere like never before - Space.com",
-        "description": "WASP-39b is now the best-explored planet outside the solar system.",
-        "url": "https://www.space.com/james-webb-space-telescope-details-exoplanet-atmosphere",
-        "urlToImage": "https://cdn.mos.cms.futurecdn.net/nTpu4XaTdZn7kDkT69rwje-1200-80.jpg",
-        "publishedAt": "2022-11-22T16:07:32Z",
-        "content": "A boiling Saturn-like planet 700 light-years away from the sun has become the best-explored planet outside our solar system. The James Webb Space Telescope's measurements of the planet's atmosphere h… [+3910 chars]"
-    },
-    {
-        "source": {
-            "id": "cnn",
-            "name": "CNN"
-        },
-        "author": "Katie Lobosco",
-        "title": "Biden's student loan forgiveness program remains blocked. Here are other ways to get debt relief - CNN",
-        "description": "President Joe Biden's student loan forgiveness program is at the mercy of the courts, and student loan borrowers could be waiting weeks, if not months, to hear whether the program is allowed to go into effect. But there are several other ways many borrowers m…",
-        "url": "https://www.cnn.com/2022/11/22/politics/biden-student-loan-forgiveness-debt-relief/index.html",
-        "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/221025113804-college-graduate-cap-stock.jpg?c=16x9&q=w_800,c_fill",
-        "publishedAt": "2022-11-22T15:48:00Z",
-        "content": "President Joe Bidens student loan forgiveness program is at the mercy of the courts, and student loan borrowers could be waiting weeks, if not months, to hear whether the program is allowed to go int… [+6763 chars]"
-    },
-    {
-        "source": {
-            "id": null,
-            "name": "The Guardian"
-        },
-        "author": "Ed Pilkington",
-        "title": "‘It feels like something different’: turning The Hours into an opera - The Guardian",
-        "description": "The acclaimed book and Oscar-winning movie, about three women grappling with despair, is making an unusual leap to the stage in New York",
-        "url": "https://www.theguardian.com/music/2022/nov/21/the-hours-opera-new-york-kevin-puts-interview",
-        "urlToImage": "https://i.guim.co.uk/img/media/fd82706634db1622efa4596f750950a3463c17d9/799_502_3277_1966/master/3277.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=20d817af4b8212ea8a35f86c4dfcdbd1",
-        "publishedAt": "2022-11-22T15:21:00Z",
-        "content": "On Tuesday night the superstar soprano Renée Fleming will return after five years to the Metropolitan Opera for the world premiere of The Hours. As a packed auditorium of her adoring fans waits with … [+8399 chars]"
-    }
+    
 ]
 //query for mourning takes out single instance
 //let query = "mourning"
 
-let query = "ftx"
-let takeOutNews = [];
-let filteredArr = []
-        testArr.forEach(element => {
-            //console.log(element.title)
-            let titleStr = element.title.toLowerCase();
-            if(titleStr.includes(query)){
+let query = "FTX"
+// let takeOutNews = [];
+// let filteredArr = []
+//         testArr.forEach(element => {
+//             //console.log(element.title)
+//             let titleStr = element.title.toLowerCase();
+//             if(titleStr.includes(query)){
                 
-                //console.log('taking out: ' + titleStr)
-                //console.log(titleStr)
-                filteredArr.push(titleStr)
-            }
+//                 //console.log('taking out: ' + titleStr)
+//                 //console.log(titleStr)
+//                 filteredArr.push(titleStr)
+//             }
             
             
-        })
-        console.log(filteredArr)
+//         })
+        //console.log(filteredArr)
             //only filters single instance
             //if filter has multiple instances nothing is added to the array
             //const filteredArr = testArr
             //testArr.filter(news => news.title.toLowerCase() == filteredArr)
-            
-
-           // const resultArr = testArr.filter(news => news.title.toLocaleLowerCase() == filteredArr)
-            //console.log(resultArr)
-            //console.log(testArr)
-            
+        //console.log(testArr)
+        let resultArr = testArr 
+        for(i=0; i<testArr.length; i++) {
+        if(testArr[i].title.includes(query)){
+        //console.log(resultArr[i])
+        resultArr.splice(i,1)
+        i--
+        }
+        //console.log(testArr[i])
+        //let resultArr = testArr.filter(news => news.title.toLowerCase() != filteredArr)
+        // return resultArr
+        }
+        //console.log(testArr)
+        console.log(resultArr)
+    
